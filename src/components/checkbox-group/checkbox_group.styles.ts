d6ed4f8368fab649bbcb857212faee74fc3aba88
Peter Carroll <petelc1966@hotmail.com>
2025-05-25 09:08:53 -0400

    checkbox group

--- src/components/checkbox-group/checkbox_group.styles.ts
+++ src/components/checkbox-group/checkbox_group.styles.ts
@@ -1,5 +1,24 @@
 import styled from '@emotion/styled';
+// import { CheckboxProps } from '../checkbox/checkbox.styles';
 
 type GroupProps = {
-  rows;
+  rows: string;
+  columns: string;
+  gap: string;
+  alignContent: string;
+  justifyContent: string;
 };
+
+export const CheckboxGroup = styled.div<GroupProps>(
+  {
+    display: 'grid',
+    width: '100%',
+  },
+  (props) => ({
+    gridTemplateRows: props.rows || 'auto',
+    gridTemplateColumns: props.columns || '1fr',
+    gap: props.gap || '8px',
+    alignContent: props.alignContent || 'start',
+    justifyContent: props.justifyContent || 'start',
+  })
+);
--- src/components/checkbox-group/checkbox_group.tsx
+++ src/components/checkbox-group/checkbox_group.tsx
@@ -4,7 +4,10 @@ type CheckboxGroupProps = {
   // Define any props you want to pass to the CheckboxGroup
   options: Array<{
     id: string;
-    value: string;
+    checked?: boolean;
+    disabled?: boolean;
+    value?: string;
+    width?: string;
     label: string;
   }>;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
@@ -22,6 +25,9 @@ export default function CheckboxGroupComponent({
           key={option.id}
           value={option.value}
           onChange={onChange}
+          checked={option.checked}
+          disabled={option.disabled}
+          //width={option}.width || 'auto' // Default width if not provided}
         />
       ))}
     </div>
